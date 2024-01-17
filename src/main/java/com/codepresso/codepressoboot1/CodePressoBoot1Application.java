ab71192acfbd77c6d2a32eeebcd8dae56b97b1b8
HaInseong <gkdlstjd1020@naver.com>
2024-01-17 13:24:18 +0900

    01-17 start

---
+++ src/main/java/com/codepresso/codepressoboot1/CodePressoBoot1Application.java
@@ -0,0 +1,13 @@
+package com.codepresso.codepressoboot1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodePressoBoot1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodePressoBoot1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/codepresso/codepressoboot1/CodePressoBoot1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codepresso.codepressoboot1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodePressoBoot1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
