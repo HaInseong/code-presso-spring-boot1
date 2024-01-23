a11f11b038aeab917ce93f728cc15b078ba55508
HaInseong <gkdlstjd1020@naver.com>
2024-01-24 07:52:05 +0900

    REST API practice

--- src/main/java/com/codepresso/codepressoboot1/controller/RequestController.java
+++ src/main/java/com/codepresso/codepressoboot1/controller/RequestController.java
@@ -11,15 +11,27 @@ import java.lang.reflect.Type;
 @RestController
 @RequestMapping(value = "/user")
 public class RequestController {
+    //내가 작성한 연습 코드
     @RequestMapping(value = "/post1")
     public String querry(@RequestParam(name = "name", required = false, defaultValue = "인성") String name,
                          @RequestParam(name = "id", required = true) String id) {
         return name + "의 아이디는 " + id + "입니다.";
     }
-
     @RequestMapping(value = "/post2/{name}/{id}")
     public String path(@PathVariable(name = "name") String name,
                        @PathVariable(name = "id") String id) {
         return name + "의 아이디는 " + id + "입니다.";
+    }
+
+    // 강사 예제 코드
+    @RequestMapping(value = "post")
+    public String getPost(@RequestParam(required = false, defaultValue = "it") String category,
+                          @RequestParam Integer id) {
+        return "you requested " + category + " - " + id + "post";
+    }
+    @RequestMapping(value = "/{type}/id/{id}")
+    public String getUser(@PathVariable(name = "type") String type,
+                          @PathVariable Integer id) {
+        return "you requested " + type + " - " + id + " user";
     }
 }
