55b45d15273173898ffaff128d54e555ece8a72d
HaInseong <gkdlstjd1020@naver.com>
2024-01-25 08:37:42 +0900

    JSON ë³서ë²클라이ì객체형 데이터 클라이언트, 서버로 보내보기 연습

--- src/main/java/com/codepresso/codepressoboot1/controller/PostController.java
+++ src/main/java/com/codepresso/codepressoboot1/controller/PostController.java
@@ -1,5 +1,6 @@
 package com.codepresso.codepressoboot1.controller;
 
+import com.codepresso.codepressoboot1.dto.PostDto;
 import org.springframework.web.bind.annotation.*;
 
 import static org.springframework.web.bind.annotation.RequestMethod.*;
@@ -17,8 +18,12 @@ public class PostController {
 
 //    @RequestMapping(method = POST)
     @PostMapping
-    public String savePost() {
-        return "POST /post";
+    public PostDto savePost(@RequestBody PostDto postDto) {
+        System.out.println(postDto.getId());
+        System.out.println(postDto.getTitle());
+        System.out.println(postDto.getContent());
+        System.out.println(postDto.getUsername());
+        return new PostDto(1, "테스트", "둘리", "희동이");
     }
 
 //    @RequestMapping(method = PUT)
