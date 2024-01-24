3e59af5584ef14dc040e0ffeecbb89337489403d
HaInseong <gkdlstjd1020@naver.com>
2024-01-24 10:11:25 +0900

    I studied PostMan Usage!

--- src/main/java/com/codepresso/codepressoboot1/controller/UserController.java
+++ src/main/java/com/codepresso/codepressoboot1/controller/UserController.java
@@ -2,7 +2,6 @@ package com.codepresso.codepressoboot1.controller;
 
 import com.codepresso.codepressoboot1.dto.SpecialtyDto;
 import com.codepresso.codepressoboot1.dto.UserDto;
-import com.fasterxml.jackson.annotation.JsonFormat;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,25 +9,27 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@RequestMapping(value = "/user")
 public class UserController {
 
-    @RequestMapping(value = "/user/paid")
+    @RequestMapping(value = "/paid")
     public String getPaidUser() {
         return "I'm a paid user.";
     }
 
-    @RequestMapping(value = "/user/enterprise")
+    @RequestMapping(value = "/enterprise")
     public String getEnterpriseUser() {
         return "I'm a enterprise user.";
     }
 
-    @RequestMapping(value = "/user")
+    @RequestMapping
     public UserDto getUser() {
         List<SpecialtyDto> specialties = new ArrayList<>();
 //        specialties.add("Java");
 //        specialties.add("Spring Boot");
         specialties.add(new SpecialtyDto("Java", "advanced"));
         specialties.add(new SpecialtyDto("Spring Boot", "Intermediate"));
+        //Check through <PostMan>!!
         return new UserDto(1, "jin", "jin@codepresso.kr", specialties);
     }
 
--- src/main/java/com/codepresso/codepressoboot1/dto/UserDto.java
+++ src/main/java/com/codepresso/codepressoboot1/dto/UserDto.java
@@ -3,7 +3,7 @@ package com.codepresso.codepressoboot1.dto;
 import java.util.List;
 
 public class UserDto {
-    Integer id;
+    Integer id; //Java의 제네릭 타입은 원시 타입을 허용하지 않습니다. 따라서, 컬렉션 등 제네릭을 사용하는 경우에는 Integer와 같은 래퍼 클래스를 사용해야 합니다.
     String name;
     String email;
     List<SpecialtyDto> specialties;
